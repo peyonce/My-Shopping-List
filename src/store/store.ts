5a5ebc3a7df04065467409b52bfe59d373b8d040
Your Name <you@example.com>
2025-09-01 12:02:35 +0200

    importing authReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import shoppingListReducer from '../shoppingList/ShoppingListSlice';
+import authReducer from '../features/userSlice'
 
 export const store = configureStore({
   reducer: {
     shoppingList: shoppingListReducer,
+    auth: authReducer
   },
 });
 
